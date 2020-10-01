67cd1115ae150113ba04f530e8d060f9badd8c52
ceylinbrooks <ceylinbrooks@gmail.com>
2020-10-01 00:14:20 -0400

    Updated HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console. log('hello world');
+var username = prompt('What is your name?');
+console. log( 'Hello' + username);
+
+var homeLocation = prompt('Am I from Atlanta?');
+var normalizedHomeLocation = homeLocation.toLowerCase();
+
+if(homeLocation === 'yes' || homeLocation === 'y')
+//console.log('You are correct' + homeLocation 'is my hometown');
+    alert('Yes! I am from Atlanta')
+else if( homeLocation === 'no' || homeLocation ==='No' )
+
+var favColor = prompt('Is my favorite color yellow?');
+var normalizedFavoriteColor = favColor.toUpperCase();
+
+if(favColor === 'no' || favColor === 'No')
+//console.log('Nope' + favColor + 'is not my favorite color')
+    alert('Nope not even close')
+
+
+var compType = prompt ('Do I use a Mac?');
+
+
