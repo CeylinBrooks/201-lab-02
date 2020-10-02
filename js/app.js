00c6b4662bde6322a75e45dee089f781c9568487
ceylinbrooks <ceylinbrooks@gmail.com>
2020-10-01 23:44:02 -0400

    initial commit, all wired

--- js/app.js
+++ js/app.js
@@ -5,38 +5,40 @@ var username = prompt('What is your name?');
 console. log( 'Hello' + username);
 
 //Q2
-var homeLocation = prompt('Am I from Atlanta?');
-var normalizedHomeLocation = homeLocation.toLowerCase();
+var homeLocation = prompt('Am I from Atlanta?').toUpperCase();
 
-if(homeLocation === 'yes' || homeLocation === 'y')
-//console.log('You are correct' + homeLocation 'is my hometown');
-    {alert('Yes! I am from Atlanta')}
-else if (homeLocation === 'no' || homeLocation ==='No' )
+if(homeLocation === 'yes' || homeLocation === 'y'){
+    alert('Yes! I am from Atlanta')
+}else if (homeLocation === 'no' || homeLocation ==='No' )
     {alert ('Actually I am!')}
-//Q3
-var favColor = prompt('Is my favorite color yellow?');
-var normalizedFavoriteColor = favColor.toUpperCase();
 
-if(favColor === 'no' || favColor === 'No')
-//console.log('Nope' + favColor + 'is not my favorite color')
-    {alert('Nope not even close')}
-else if (favColor === 'yes' || favColor === 'YES')
+//console.log('You are correct' + homeLocation 'is my hometown');
+
+//Q3
+var favColor = prompt('Is my favorite color yellow?').toUpperCase();
+
+if(favColor === 'no'){
+    alert('Nope not even close')
+}else if (favColor === 'yes')
     {alert( 'You are correct, it is purple though!')}
 
+//console.log('Nope' + favColor + 'is not my favorite color')
+
 //Q4
-var compType = prompt ('Do I use a Mac?').toLowerCase();
-if(compType === 'no' )
-// console.log (compType + 'I have a Windows machine')
-    {alert('Right because I use a Windows machine')}
-else if ( compType === 'yes' || compType === 'y' )
+var compType = prompt ('Do I use a Mac?').toUpperCase();
+
+if(compType === 'no' ){
+    alert('Right because I use Windows')
+} else if ( compType === 'yes' || compType === 'y' )
     {alert('Try Again :(')}
+
+// console.log (compType + 'I have a Windows machine')
 
 //Q5
 
-var favAnimal = prompt ('Is my most animal in the world, an Elephant?');
-var normalizedFavAnimal = favAnimal.toUpperCase();
+var favAnimal = prompt ('Is my most favorite animal in the world, an Elephant?').toUpperCase();
 
-if (favAnimal === 'Yes' || favAnimal === 'yes' || favAnimal === 'YES') {
+if ( favAnimal === 'yes') {
     alert('Absolutely, they are the best!');
 } else if (favAnimal === 'no' || favAnimal === 'NO') {
     alert('Are you kidding, Elephants are fantastic!');
